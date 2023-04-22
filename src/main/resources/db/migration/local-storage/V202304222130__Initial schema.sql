0337d69257c920812f5b29a956df714f60c1aa0e
sibmaks <sibmaks@vk.com>
2023-04-22 22:43:42 +0300

    Fix H2 syntax

--- src/main/resources/db/migration/local-storage/V202304222130__Initial schema.sql
+++ src/main/resources/db/migration/local-storage/V202304222130__Initial schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS STORAGE_CONTENT
 CREATE TABLE IF NOT EXISTS STORAGE_CONTENT_META
 (
     id         bigint auto_increment PRIMARY KEY,
-    key        varchar(255)  NOT NULL,
-    value      varchar(2048) NOT NULL,
+    "key"      varchar(255)  NOT NULL,
+    "value"    varchar(2048) NOT NULL,
     content_id varchar(255)  NOT NULL,
     foreign key (content_id) references STORAGE_CONTENT (uid)
 );
