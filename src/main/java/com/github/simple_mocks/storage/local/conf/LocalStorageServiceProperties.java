447ad3a0fab041a1269e8c3cd18e0457ed9042ee
sibmaks <sibmaks@vk.com>
2024-04-23 15:35:57 +0300

    LocalStorageServiceProperties add setters

--- src/main/java/com/github/simple_mocks/storage/local/conf/LocalStorageServiceProperties.java
+++ src/main/java/com/github/simple_mocks/storage/local/conf/LocalStorageServiceProperties.java
@@ -4,6 +4,7 @@ import com.github.simple_mocks.storage.local.dto.ContentStorageFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @author sibmaks
  * @since 0.1.0
  */
+@Setter
 @Getter
 @Configuration
 @NoArgsConstructor
